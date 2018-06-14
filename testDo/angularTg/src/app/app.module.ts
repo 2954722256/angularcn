fe9ef082481e1f19a031d9d4091f375ec4aa7e52
dodo_lihao <2954722256@qq.com>
2018-06-14 14:36:43 +0800

    move

--- testDo/angularTg/src/app/app.module.ts
+++ testDo/angularTg/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { CardHeaderComponent } from './card-header/card-header.component';
+import { CardComponent } from './component/card/card.component';
+import { CardHeaderComponent } from './component/card-header/card-header.component';
 
 @NgModule({
   declarations: [
