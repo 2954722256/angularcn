e25d4085af0d6fd96d31befdf387878d0af20863
dodo_lihao <2954722256@qq.com>
2018-06-14 13:54:46 +0800

    card-header component

--- testDo/angularTg/src/app/app.module.ts
+++ testDo/angularTg/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CardHeaderComponent } from './card-header/card-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CardHeaderComponent
   ],
   imports: [
     BrowserModule
