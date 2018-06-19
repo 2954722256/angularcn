b51446d2bb62728e28d3c31df31dcbe1c07fb4bf
dodo_lihao <2954722256@qq.com>
2018-06-19 13:40:06 +0800

    简单提取方法

--- testDo/angular2/src/app/sheetjs.component.ts
+++ testDo/angular2/src/app/sheetjs.component.ts
@@ -42,7 +42,25 @@ export class SheetJSComponent {
 	}
 
 
-	
+    import02getCell(evt: any){
+        /* wire up file reader */
+        const target: DataTransfer = <DataTransfer>(evt.target);
+        if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+        const reader: FileReader = new FileReader();
+        reader.onload = (e: any) => {
+            /* 读取整个excel 		：	read workbook */
+            const bstr: string = e.target.result;
+            const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+
+            /* 拿到第一页sheet   ： grab first sheet */
+            const wsname: string = wb.SheetNames[0];
+            const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+            /* save data */
+            this.data = <AOA>(XLSX.utils.sheet_to_json(ws, {header: 1}));
+        };
+        reader.readAsBinaryString(target.files[0]);
+    }
 
 
 
